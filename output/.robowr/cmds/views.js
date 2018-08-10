a62ed3f9e3dd60975c118270e6d3df09f5eeede0
Tero Tolonen <tero@sumpli.com>
2018-08-10 14:58:45 +0300

    robowr

--- output/.robowr/cmds/views.js
+++ output/.robowr/cmds/views.js
@@ -1,4 +1,11 @@
 
 module.exports.run = function ( wr ) {
   console.log('Views called ')
+  const model = wr.getState()
+  const viewsFile = wr.getFileWriter('/src/views/', 'index.ts');
+
+  viewsFile.out('', true)
+  model.entities.forEach( entity => {
+    viewsFile.out(`// view for ${entity.name}`, true)
+  })
 }
