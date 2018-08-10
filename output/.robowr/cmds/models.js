81d37ebc2db9f63d44e0c763b0d1ec5660ce9ae3
Tero Tolonen <tero@sumpli.com>
2018-08-10 14:53:51 +0300

    robowr

---
+++ output/.robowr/cmds/models.js
@@ -0,0 +1,10 @@
+
+
+// create base models for the application...
+module.exports.run = function ( wr ) {
+  const model = wr.getState()
+  model.entities.forEach( entity => {
+    const m = wr.getFileWriter('/src/model/', `${entity.name}.ts`)
+    m.out('// Model definition for ' + entity.name, true)
+  })
+}
