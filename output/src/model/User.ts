900e68da8689ccf7c78d0766ba0ad7dad448a2a9
Tero Tolonen <tero@sumpli.com>
2018-08-11 11:30:47 +0300

    robowr

--- output/src/model/User.ts
+++
@@ -1,5 +0,0 @@
-// Model definition for User
-export class ModelUser {
-  first_name : string
-  last_name : string
-}
---
+++ output/src/model/Users.ts
@@ -0,0 +1,5 @@
+// Model definition for Users
+export class ModelUsers {
+  first_name : string
+  last_name : string
+}
